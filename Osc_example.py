889c1a582472201ca415f38b8c039b7595551264
XiongStony <963877409@qq.com>
2025-05-28 19:07:19 -0400

    Update Osc_example.py

--- Osc_example.py
+++ Osc_example.py
@@ -1,4 +1,4 @@
-# This is a example of doing once test using osc instrument include generate a impulse signal and get the response data and print data
+# This is a example of doing once test using osc instrument include generate a impulse signal and get the response data and plot data
 from moku.instruments import Oscilloscope
 import numpy as np
 import pandas as pd
@@ -18,10 +18,14 @@ try:
     Osc.generate_waveform(1, 'Pulse',pulse_width=1e-6,edge_time=0.5e-6, amplitude=10, frequency=300)
     Osc.set_source(1,'Input1')
 
-    # Get and print a single frame of data (time series
-    # of voltage per channel)
-    data = Osc.get_data(wait_complete= True) # doing this is only for geting the time vector
-    avertest = osc_test_np(Osc,'ch1',20) # this is calling the function from mokulib file 
+    # this is only for geting the time vector you can basicly ingnor this if you dont need to plot value versus time
+    data = Osc.get_data(wait_complete= True)
+    
+    # this is calling the function from mokulib file wrote by myself
+    #'ch1' mean the data recived from the first chanel(we only need first chael of the moku both for the input and output) 
+    # 20 means taking 20 times test and calculating average of them as the result
+    # and it will automaticly return a numpy array
+    avertest = osc_test_np(Osc,'ch1',20) 
     print(data['time'][-1])
     print(len(data['time']))
     plt.plot(data['time'],avertest)
